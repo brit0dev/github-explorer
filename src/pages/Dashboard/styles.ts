5d132b8434adde3c23186c8352206f07500e62e1
brito <britoifpe@gmail.com>
2025-02-18 16:10:58 -0500

    Project files

---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,116 @@
+import styled, { css } from 'styled-components';
+import { shade } from 'polished';
+
+type FormProps = {
+  hasError: boolean;
+};
+
+export const Title = styled.h1`
+  font-size: 48px;
+  color: #3a3a3a;
+  max-width: 450px;
+  line-height: 56px;
+
+  margin-top: 80px;
+`;
+
+export const Form = styled.form<FormProps>`
+  margin-top: 40px;
+  max-width: 700px;
+
+  display: flex;
+
+  input {
+    flex: 3;
+    height: 70px;
+    padding: 0 24px;
+    border: 0;
+    border-radius: 5px 0 0 5px;
+    min-width: 3em;
+    color: #a3a3a3;
+    border: 1px solid #fff;
+    border-right: 0;
+
+    ${(props) =>
+      props.hasError &&
+      css`
+        border-color: #c53030;
+      `}
+
+    &::placeholder {
+      color: '#a8a8b3';
+    }
+  }
+  button {
+    flex: 1;
+    background: #04d361;
+    border-radius: 0px 5px 5px 0px;
+    border: 0;
+    color: #fff;
+    font-weight: bold;
+    min-width: 8em;
+    transition: background-color 0.2s;
+
+    &:hover {
+      background: ${shade(0.1, '#04d361')};
+    }
+  }
+`;
+
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin-top: 8px;
+`;
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+
+    text-decoration: none;
+    color: inherit;
+
+    display: flex;
+    align-items: center;
+
+    & + a {
+      margin-top: 14px;
+    }
+
+    &:hover {
+      background: ${shade(0.02, '#fff')};
+    }
+    svg {
+      transition: transform 0.2s;
+    }
+    &:hover svg {
+      transform: translateX(7px);
+    }
+
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+
+    div {
+      flex: 1;
+      margin-left: 10px;
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 3px;
+      }
+    }
+  }
+`;
---
+++ src/pages/Repository/index.tsx
@@ -0,0 +1,108 @@
+import React, { useEffect, useState } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+import { Header, RepositoryInfo, Issues } from './styles';
+import api from '../../services/api';
+
+type RepositoryParams = {
+  repository: string;
+  owner: string;
+};
+type RepositoryType = {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+  stargazers_count: number;
+  forks_count: number;
+  open_issues_count: number;
+};
+
+type Issue = {
+  id: number;
+  title: string;
+  html_url: string;
+  user: {
+    login: string;
+  };
+};
+
+const Repository: React.FC = () => {
+  const params = useParams<RepositoryParams>();
+  const repositoryFullName = `${params.owner}/${params.repository}`;
+
+  const [repository, setRepository] = useState<RepositoryType | null>(null);
+  const [issues, setIssues] = useState<Issue[]>([]);
+
+  useEffect(() => {
+    api.get(`repos/${repositoryFullName}`).then((response) => {
+      setRepository(response.data);
+    });
+
+    api.get(`repos/${repositoryFullName}/issues`).then((response) => {
+      setIssues(response.data);
+    });
+  }, [params]);
+
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+      {repository && (
+        <RepositoryInfo>
+          <header>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+          </header>
+          <ul>
+            <li>
+              <strong>{repository.stargazers_count}</strong>
+              <span>Stars</span>
+            </li>
+            <li>
+              <strong>{repository.forks_count}</strong>
+              <span>Forks</span>
+            </li>
+            <li>
+              <strong>67</strong>
+              <span>Issues abertas</span>
+            </li>
+          </ul>
+        </RepositoryInfo>
+      )}
+
+      <Issues>
+        {issues.map((issue) => (
+          <Link key={issue.id} to={issue.html_url} target="_blank">
+            <div>
+              <strong>{issue.title}</strong>
+              <p>{issue.user.login}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </Link>
+        ))}
+      </Issues>
+    </>
+
+    // <h1>
+    //   Repository: {params.owner}/{params.repository}
+    // </h1>
+  );
+};
+
+export default Repository;
